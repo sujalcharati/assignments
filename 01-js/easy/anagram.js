2a655cc94f4656d5eaaeee7e54094ff832a921d7
sujalcharati <sujalc024@gmail.com>
2024-08-12 17:45:02 +0530

    test cases

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,15 +4,11 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
+
 function isAnagram(str1, str2) {
-  str1="sujal";
-  str2="jalus";
-  if(str1==str2){
-    return false;
-  }
-  else{
-    return true;
-  }
+  str1 = str1.toLowerCase().split('').sort().join('').trim();
+  str2 = str2.toLowerCase().split('').sort().join('').trim();
+  return str1 === str2;
 }
 
 module.exports = isAnagram;
