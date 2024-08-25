d4e5a58057aa70f3ae3dadde2cc3f83156641a4c
sujalcharati <sujalc024@gmail.com>
2024-08-25 11:27:53 +0530

    update

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -22,4 +22,10 @@ app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
 
+app.use((err,req,res,next)=> {
+  requestCount = requestCount+1;
+  res.send(`${ requestCount}`)
+
+})
+ app.listen(3000);
 module.exports = app;
